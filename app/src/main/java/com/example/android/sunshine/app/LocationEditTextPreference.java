f2eed40d5e2b58a6bb496244d37f1543445575cc
José Bernardino da Silva Júnior <jbernardino.jr@gmail.com>
2017-10-31 22:27:45 -0200

    Implementation of minLength in custom EditTextPreference
    This commit add the TextWatcher in the location edit text settings

--- app/src/main/java/com/example/android/sunshine/app/LocationEditTextPreference.java
+++ app/src/main/java/com/example/android/sunshine/app/LocationEditTextPreference.java
@@ -1,11 +1,18 @@
 package com.example.android.sunshine.app;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.os.Bundle;
 import android.preference.EditTextPreference;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by root on 30/10/17.
@@ -34,5 +41,36 @@ public class LocationEditTextPreference extends EditTextPreference {
     protected View onCreateDialogView() {
         Log.d("Bernardino", "minLength = " + mMinLength);
         return super.onCreateDialogView();
+    }
+
+    @Override
+    protected void showDialog(Bundle state){
+        super.showDialog(state);
+        EditText et = getEditText();
+
+        et.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                Button positiveButton;
+                Dialog dialog = getDialog();
+                if (dialog instanceof AlertDialog) {
+                    AlertDialog alertDialog = (AlertDialog) dialog;
+                    positiveButton = alertDialog.getButton(AlertDialog.BUTTON_POSITIVE);
+                    if (editable.length() >= mMinLength) {
+                        positiveButton.setEnabled(true);
+                    } else {
+                        positiveButton.setEnabled(false);
+                    }
+                }
+            }
+        });
     }
 }
